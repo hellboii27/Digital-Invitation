59989726931dbbc32fdc4aba94f08d0c8020be77
bayy <b.wicaksono18@gmail.com>
2026-02-04 10:34:09 +0700

    update main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # FastAPI setup
 from fastapi import FastAPI
+from app.routers.invitation import router as invitation_router
 
 # Initialize FastAPI
 app = FastAPI(title="Digital Invitation API")
@@ -10,3 +11,6 @@ def health_check():
     return {
         "status": "ok",
         "message": "Digital Invitation API is running"    }
+
+# Invitation router
+app.include_router(invitation_router)
